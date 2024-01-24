008e157a0d5e4200eeb30952b4cd609efec607a7
osmantirpan <osmantirpan@hacettepe.edu.tr>
2024-01-24 09:54:08 +0300

    sign out

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,18 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class MainScreen extends StatefulWidget{
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _MainScreenState();
   }
-
-
 }
-class _MainScreenState extends State<MainScreen>{
+
+class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center();
+    return Scaffold(
+      appBar: AppBar(actions: [
+        IconButton(
+            onPressed: () async => await FirebaseAuth.instance.signOut(),
+            icon: const Icon(Icons.logout))
+      ]),
+    );
   }
 }
