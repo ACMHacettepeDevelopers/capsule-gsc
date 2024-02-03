119b5c37c7f5ce07dd9bed16980ec9fee1f0b2ff
osmantirpan <osmantirpan@hacettepe.edu.tr>
2024-02-03 08:49:03 +0300

    chat screen initial build

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,20 +1,44 @@
+import 'dart:convert';
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 class ChatScreen extends StatefulWidget {
-  const ChatScreen({super.key});
-
+  const ChatScreen({Key? key}) : super(key: key);
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 class _ChatScreenState extends State<ChatScreen>{
+  final List<types.Message> _messages = [];
+  final _user = const types.User(id: '82091008-a484-4a89-ae75-a22bf8d6f3ac');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Chat"),
       ),
-      body: const Center(
-        child: Text("Chat Screen"),
-      ),
+      body: Chat(messages: [], onSendPressed: _handleSendPressed, user: _user,)
     );
   }
+  void _addMessage(types.Message message) {
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
+  void _handleSendPressed(types.PartialText message) {
+    final textMessage = types.TextMessage(
+      author: _user,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: randomString(),
+      text: message.text,
+    );
+
+    _addMessage(textMessage);
+  }
+  String randomString() {
+  final random = Random.secure();
+  final values = List<int>.generate(16, (i) => random.nextInt(255));
+  return base64UrlEncode(values);
+}
 }
