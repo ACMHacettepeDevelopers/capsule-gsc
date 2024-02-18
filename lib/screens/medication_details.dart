06b7c87f7c74a061fd9c6f7b4420d4f0c1351db9
osmantirpan <osmantirpan@hacettepe.edu.tr>
2024-02-18 22:41:44 +0300

    code enhances

--- lib/screens/medication_details.dart
+++ lib/screens/medication_details.dart
@@ -1,10 +1,13 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:capsule_app/models/medication.dart';
 import 'package:capsule_app/screens/calendar.dart';
+import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class MedicationDetails extends StatefulWidget {
-  const MedicationDetails({Key? key, this.medication}) : super(key: key);
+  const MedicationDetails({Key? key, required this.medication}) : super(key: key);
   final Medication? medication;
 
   @override
@@ -12,6 +15,7 @@ class MedicationDetails extends StatefulWidget {
 }
 
 class _MedicationDetailsState extends State<MedicationDetails> {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,12 +25,12 @@ class _MedicationDetailsState extends State<MedicationDetails> {
       ),
       body: SingleChildScrollView(
         child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Title_Text(widget.medication!.name),
-              SizedBox(
+              const SizedBox(
                 height: 5,
               ),
               Container(
@@ -35,37 +39,38 @@ class _MedicationDetailsState extends State<MedicationDetails> {
                 width: 240,
                 child: Center(
                   child: Text(
-                    "From ${widget.medication?.dayAdded}-- To ${widget.medication?.endDay}",
-                    style: TextStyle(
+                    "From ${DateFormat('yyy-MM-dd').format(widget.medication!.dayAdded)}-- To ${DateFormat('yyy-MM-dd').format(widget.medication!.endDay)}",
+                    style: const TextStyle(
                         color: Colors.black54, fontWeight: FontWeight.w800),
                   ),
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 15,
               ),
-              SizedBox(
+              const SizedBox(
                 height: 15,
               ),
               subTitle("Medication Dosage"),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
-              AboutMedicine(widget.medication!.name, widget.medication!.dose),
-              SizedBox(
+              aboutMedicine(widget.medication!),
+              const SizedBox(
                 height: 15,
               ),
               subTitle("Medication Time"),
-              SizedBox(
+              const SizedBox(
                 height: 10,
               ),
-              AboutMedicine(
-                  widget.medication!.name, widget.medication!.times + " times"),
-              SizedBox(
+              aboutMedicine(
+                   widget.medication
+                   !),
+              const SizedBox(
                 height: 15,
               ),
-              Container(
-                height: 400,
+              SizedBox(
+                height: 500,
                 child: Table_Calender(medication: widget.medication!),
               )
             ],
@@ -79,22 +84,22 @@ class _MedicationDetailsState extends State<MedicationDetails> {
 Container Title_Text(String title) {
   return Container(
     height: 40,
-    padding: EdgeInsets.symmetric(horizontal: 16),
+    padding: const EdgeInsets.symmetric(horizontal: 16),
     decoration: BoxDecoration(
-      boxShadow: [
-        BoxShadow(
+      boxShadow: const [
+         BoxShadow(
           color: Colors.black,
           offset: Offset(4, 4),
           blurRadius: 10,
         ),
       ],
       color: Colors.amber[200],
-      borderRadius: BorderRadius.all(Radius.circular(20)),
+      borderRadius: const BorderRadius.all(Radius.circular(20)),
     ),
     child: Center(
       child: Text(
         "More Information About ${title}",
-        style: TextStyle(
+        style: const TextStyle(
           fontSize: 20,
           fontWeight: FontWeight.bold,
           color: Colors.black87,
@@ -104,21 +109,24 @@ Container Title_Text(String title) {
   );
 }
 
-Container AboutMedicine(String name, String details) {
+Container aboutMedicine(Medication medication) {
+  final name = medication.name;
+  final times = jsonDecode(medication.times) as Map<String, dynamic>;
+  final timesArray = times.keys.toList();
   return Container(
     decoration: BoxDecoration(
       color: Colors.green[200],
-      borderRadius: BorderRadius.all(Radius.circular(20)),
+      borderRadius: const BorderRadius.all(Radius.circular(20)),
     ),
     height: 40,
-    width: "${name} has ${details} in a day. You do not forget it"
+    width: "$name has ${medication.dose} doses in a day. You do not forget it"
             .length
             .toDouble() *
         10,
     child: Center(
       child: Text(
-        "${name} has ${details} in a day. You do not forget it",
-        style: TextStyle(
+        "$name has to be taken at ${timesArray.join(", ")}",
+        style: const TextStyle(
           fontWeight: FontWeight.w700,
           color: Colors.black87,
         ),
@@ -130,7 +138,7 @@ Container AboutMedicine(String name, String details) {
 Widget subTitle(String title) {
   return Text(
     title,
-    style: TextStyle(
+    style: const TextStyle(
       fontSize: 20,
       fontWeight: FontWeight.bold,
       color: Colors.black87,
